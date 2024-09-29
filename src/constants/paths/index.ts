104a1c217c14f63e70c6f1d77f1da3fbd5b9529a
Raza Haider <razah12145@gmail.com>
2024-09-30 00:01:59 +0500

    paths

--- src/constants/paths/index.ts
+++ src/constants/paths/index.ts
@@ -1,7 +1,7 @@
 import path from "./types";
 
-export const BASE_PATH : path = "/";
-export const ADMIN_PATH : path = "/admin";
-export const ADMIN_LOGIN_PATH : path= "/admin/login";
-export const ADMIN_REGISTRATION_LISTING_PATH : path = "/admin/registration-listing";
-export const NOT_FOUND_PATH = '/404'
+export const BASE_PATH : path = "/mercantile";
+export const ADMIN_PATH : path = `${BASE_PATH}/admin`;
+export const ADMIN_LOGIN_PATH : path= `${BASE_PATH}/admin/login`;
+export const ADMIN_REGISTRATION_LISTING_PATH : path = `${BASE_PATH}/admin/registration-listing`;
+export const NOT_FOUND_PATH = `${BASE_PATH}/404`
