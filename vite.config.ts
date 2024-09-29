147a821ecaceeeac507e5392cb74e9438cdbab6b
Raza Haider <razah12145@gmail.com>
2024-09-29 23:52:23 +0500

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import vitePluginImp from 'vite-plugin-imp';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: [
-        'antd/es/layout/style/index.scss',
-        './src/styles/antd.theme.scss',
-      ]
-    }
-  },
   plugins: [
     react(),
-    vitePluginImp({
-      libList: [
-        {
-          libName: 'antd',
-          style: (name) => `antd/es/${name}/style/index.scss`,
-        },
-      ],
-    }),
   ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: ``,
-      },
-    }
-  },
 });
